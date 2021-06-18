8a2ee8270627e0e514d05d40c18ef184bc981c3a
Greg DiCristofaro <gregdicristofaro@gmail.com>
2021-06-17 20:50:52 -0400

    updates

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import EventsDemo from './EventsDemo';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
